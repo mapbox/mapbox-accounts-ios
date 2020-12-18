5caab6d26ceb9792e04375e10c4d6a244dc64c49
Minh Nguyễn <mxn@1ec5.org>
2020-12-17 18:41:55 -0800

    v2.3.1

--- Package.swift
+++ Package.swift
@@ -3,11 +3,12 @@
 
 import PackageDescription
 
-let version = "2.3.0"
-let checksum = "43dcba1b273d07f97e3ef1aea79bfc0a7094a175bb7f73aa9b5eb53733caf75e"
+let version = "2.3.1"
+let checksum = "2057e58ed24eb2f8ca7ebccebd848093d8df1a56c95473a424942e12d73bc8bd"
 
 let package = Package(
     name: "mapbox-accounts-ios",
+    platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -21,7 +22,6 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .binaryTarget(name: "MapboxAccounts", url: "https://mapbox.com/ios-sdk/MapboxAccounts-\(version).zip", checksum: checksum),
-    ],
-    cxxLanguageStandard: .cxx14
+        .binaryTarget(name: "MapboxAccounts", url: "https://www.mapbox.com/ios-sdk/MapboxAccounts-\(version).xcframework.zip", checksum: checksum),
+    ]
 )
