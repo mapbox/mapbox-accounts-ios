cd438c7a5d41c520379a09d12340124c980a7225
Minh Nguyễn <mxn@1ec5.org>
2020-12-19 21:38:28 -0800

    Fixed package name

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let version = "2.3.1"
 let checksum = "2057e58ed24eb2f8ca7ebccebd848093d8df1a56c95473a424942e12d73bc8bd"
 
 let package = Package(
-    name: "mapbox-accounts-ios",
+    name: "MapboxAccounts",
     platforms: [.iOS(.v9)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
